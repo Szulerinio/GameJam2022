33c739b42368fd8c1b6386ebc71d842eaa1368e3
Szymon Kozłowski <szymonkozlowski48@gmail.com>
2022-10-21 17:14:25 +0200

    keeping track of pressed keys

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,72 @@
-import { setupCounter } from './counter'
+import { setupCounter } from './counter';
 
-const canvas = document.querySelector<HTMLDivElement>('#canvas')
+const canvas = document.querySelector<HTMLDivElement>('#canvas');
 
-const onKeyDown= ()=>{}
+const keysPressed = {
+  a: true,
+  s: true,
+  d: true,
+  w: true
+};
 
-document.addEventListener('keydown', onKeyDown)
-document.addEventListener('keyup', ()=>{})
-document.addEventListener('mousedown', ()=>{})
-document.addEventListener('mouseup', ()=>{})
+const onKeyDown = (e: KeyboardEvent) => {
+  switch (e.key) {
+    case 'a':
+    case 'A':
+      keysPressed.a = true;
+      break;
+    case 'd':
+    case 'D':
+      keysPressed.d = true;
+      break;
+    case 's':
+    case 'S':
+      keysPressed.s = true;
+      break;
+    case 'w':
+    case 'W':
+      keysPressed.w = true;
+      break;
+    default:
+      break;
+  }
+};
+
+const onKeyUp = (e: KeyboardEvent) => {
+  switch (e.key) {
+    case 'a':
+    case 'A':
+      keysPressed.a = false;
+      break;
+    case 'd':
+    case 'D':
+      keysPressed.d = false;
+      break;
+    case 's':
+    case 'S':
+      keysPressed.s = false;
+      break;
+    case 'w':
+    case 'W':
+      keysPressed.w = false;
+      break;
+    default:
+      break;
+  }
+};
+const onMouseDown = () => {
+  console.log('aaa');
+};
+const onMouseUp = () => {
+  console.log('aaa');
+};
+
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
+document.addEventListener('mousedown', onMouseDown);
+document.addEventListener('mouseup', onMouseUp);
 // document.addEventListener('mousemove', ()=>{})
 
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const render = () => {};
